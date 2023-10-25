ecde73b874a57a5be0d259333069f4ca05a08527
Douglas Weaver <douglasweaver@mac.com>
2023-10-25 11:29:52 -0400

    add custom query

--- src/graphql/query_bookingsByCheckIn.js
+++ src/graphql/query_bookingsByCheckIn.js
@@ -1,47 +1,4 @@
 /* eslint-disable */
-// this is an auto generated file. This will be overwritten
-
-export const getVDSBooking = /* GraphQL */ `
-  query GetVDSBooking($id: ID!) {
-    getVDSBooking(id: $id) {
-      id
-      guests
-      description
-      checkIn
-      checkOut
-      levels
-      autos
-      commitment
-      type
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listVDSBookings = /* GraphQL */ `
-  query ListVDSBookings(
-    $filter: ModelVDSBookingFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listVDSBookings(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        guests
-        description
-        checkIn
-        checkOut
-        levels
-        autos
-        commitment
-        type
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
 export const bookingsByCheckIn = /* GraphQL */ `
   query BookingsByCheckIn(
     $type: String!
