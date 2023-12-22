3417d4714f33fef1ec94cd167cebe8bdabd27010
Douglas Weaver <douglasweaver@mac.com>
2023-12-22 14:56:23 -0500

    rebuild entire backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,41 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "AdminQueries": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
     "vdssurf": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
-  "auth": {
-    "userPoolGroups": {
-      "VDSSurfOwnersUserPoolGroupRole": "string"
-    },
-    "vdssurfeed6ea65": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "AdminQueriesbae648e0": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+      "GraphQLAPIIdOutput": "string"
     }
   },
   "storage": {
-    "imagestorage": {
+    "vdss3": {
       "BucketName": "string",
       "Region": "string"
     }
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -47,6 +47,42 @@ export const listVDSBookings = /* GraphQL */ `
     }
   }
 `;
+export const getVDSNote = /* GraphQL */ `
+  query GetVDSNote($id: ID!) {
+    getVDSNote(id: $id) {
+      id
+      name
+      comments
+      fileName
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const listVDSNotes = /* GraphQL */ `
+  query ListVDSNotes(
+    $filter: ModelVDSNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listVDSNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        comments
+        fileName
+        createdAt
+        updatedAt
+        owner
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
 export const VDSBookingsByDate = /* GraphQL */ `
   query VDSBookingsByDate(
     $type: String!
@@ -74,42 +110,6 @@ export const VDSBookingsByDate = /* GraphQL */ `
         autos
         commitment
         type
-        createdAt
-        updatedAt
-        owner
-        __typename
-      }
-      nextToken
-      __typename
-    }
-  }
-`;
-export const getVDSNote = /* GraphQL */ `
-  query GetVDSNote($id: ID!) {
-    getVDSNote(id: $id) {
-      id
-      name
-      comments
-      fileName
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const listVDSNotes = /* GraphQL */ `
-  query ListVDSNotes(
-    $filter: ModelVDSNoteFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listVDSNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        comments
-        fileName
         createdAt
         updatedAt
         owner
